32901c3f3c58f6f5d4402ae074bf1d733f977841
wdhg <wdhgrant@gmail.com>
2020-05-19 15:27:55 +0100

    add checking if the game has been won

--- tic_tac_toe/tictactoe.c
+++ tic_tac_toe/tictactoe.c
@@ -46,7 +46,60 @@ void player_turn(enum tile player, enum tile *board) {
   board[x + y * BOARD_SIZE] = player;
 }
 
-int player_has_won(enum tile *board) { return 0; }
+int check_rows(enum tile *board) {
+  for (int y = 0; y < BOARD_SIZE; y++) {
+    if (board[y * BOARD_SIZE] == Blank)
+      continue;
+    int won = 1;
+    for (int x = 0; x < BOARD_SIZE; x++) {
+      won &= board[x + y * BOARD_SIZE] == board[y * BOARD_SIZE];
+    }
+    if (won)
+      return 1;
+  }
+  return 0;
+}
+
+int check_cols(enum tile *board) {
+  for (int x = 0; x < BOARD_SIZE; x++) {
+    if (board[x] == Blank)
+      continue;
+    int won = 1;
+    for (int y = 0; y < BOARD_SIZE; y++) {
+      won &= board[x + y * BOARD_SIZE] == board[x];
+    }
+    if (won)
+      return 1;
+  }
+  return 0;
+}
+
+int check_diag(enum tile *board) {
+  // check main diagonal
+  if (board[0] != Blank) {
+    int won = 1;
+    for (int x = 1; x < BOARD_SIZE; x++) {
+      won &= board[x + x * BOARD_SIZE] == board[0];
+    }
+    if (won)
+      return 1;
+  }
+  // check antidiagonal
+  if (board[BOARD_SIZE - 1] != Blank) {
+    int won = 1;
+    for (int x = 1; x < BOARD_SIZE; x++) {
+      won &=
+          board[(BOARD_SIZE - 1 - x) + x * BOARD_SIZE] == board[BOARD_SIZE - 1];
+    }
+    if (won)
+      return 1;
+  }
+  return 0;
+}
+
+int player_has_won(enum tile *board) {
+  return check_rows(board) || check_cols(board) || check_diag(board);
+}
 
 int main(int argc, char *argv[]) {
   enum tile board[BOARD_SIZE * BOARD_SIZE];
@@ -57,6 +110,7 @@ int main(int argc, char *argv[]) {
     display_board(board);
     player_turn(player, board);
     if (player_has_won(board)) {
+      display_board(board);
       printf("PLAYER %s HAS WON\n", player == Naught ? "O" : "X");
       break;
     }
